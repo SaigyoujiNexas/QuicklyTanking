715cc456f4a6abbe3dc99ab3ebb68dfcc4399aef
Saigyouji Nexas <saigyoujinexas@gmail.com>
2022-01-06 23:44:34 +0800

    Using ARouter

--- app/src/main/java/com/example/quicklytanking/MainActivity.java
+++ app/src/main/java/com/example/quicklytanking/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.alibaba.android.arouter.facade.annotation.Route;
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
