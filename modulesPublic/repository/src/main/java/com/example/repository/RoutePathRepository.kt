928bd4cac493daf0ae535cf57623352ef356f368
Saigyouji Nexas <saigyoujinexas@gmail.com>
2022-01-08 13:45:39 +0800

    using ARouter jump support

--- modulesPublic/repository/src/main/java/com/example/repository/RoutePathRepository.kt
+++ modulesPublic/repository/src/main/java/com/example/repository/RoutePathRepository.kt
@@ -3,7 +3,7 @@ package com.example.repository
 
 class RoutePathRepository {
     companion object{
-        const val APP = "/main/StartActivity"
+        const val APP = "/app/StartActivity"
         const val LOG_IN = "/login/LoginActivity"
         const val MAIN = "/main/MainActivity"
     }
