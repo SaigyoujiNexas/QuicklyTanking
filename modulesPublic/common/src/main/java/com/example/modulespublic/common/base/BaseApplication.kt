d96d20f61a8c3da3c519430b6aa36c5f697d1aa5
Saigyouji Nexas <saigyoujinexas@gmail.com>
2022-01-15 21:13:42 +0800

    add the logger support and properties support.

--- modulesPublic/common/src/main/java/com/example/modulespublic/common/base/BaseApplication.kt
+++ modulesPublic/common/src/main/java/com/example/modulespublic/common/base/BaseApplication.kt
@@ -1,12 +1,18 @@
 package com.example.modulespublic.common.base
 
 import android.app.Application
+import android.text.TextUtils
 import com.alibaba.android.arouter.launcher.ARouter
+import com.example.modulesbase.libbase.util.PropertiesUtil
 
 open class BaseApplication : Application() {
     override fun onCreate() {
-        ARouter.openDebug()
-        ARouter.openLog()
+        val isRelease = PropertiesUtil.props.getProperty("isRelease")
+        if(!TextUtils.equals(isRelease, "true"))
+        {
+            ARouter.openLog()
+            ARouter.openDebug()
+        }
         ARouter.init(this)
         super.onCreate()
     }
