a5e945d9a30299f764717dbd996f17a278b7ce7c
zprjd <2637808037@qq.com>
2022-02-25 20:16:49 +0800

    add N' adapt ProgressButton

--- modulesCore/main/src/main/java/com/example/modulescore/main/RunActivity.java
+++ modulesCore/main/src/main/java/com/example/modulescore/main/RunActivity.java
@@ -1,20 +1,32 @@
 package com.example.modulescore.main;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class RunActivity extends BaseActivity implements View.OnClickListener {
     FloatingActionButton startRunButton;
     FloatingActionButton stopRunButton;
-    FloatingActionButton finishRunButton;
+    ProgressButton finishRunButton;
     CardView toMapCard;
+    ConstraintLayout constraintLayout_run;
     boolean isRun = true;
+    TextView animationText;
+    ImageView animationBackGround;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +35,9 @@ public class RunActivity extends BaseActivity implements View.OnClickListener {
         stopRunButton = findViewById(R.id.stopRunButton);
         finishRunButton = findViewById(R.id.finishRunButton);
         toMapCard = findViewById(R.id.toMapCard);
+        animationText = findViewById(R.id.text_animation);
+        animationBackGround = findViewById(R.id.animationBackGround);
+        constraintLayout_run = findViewById(R.id.constraintLayout_Run);
         startRunButton.hide();
         finishRunButton.hide();
         //设置监听器
@@ -30,25 +45,18 @@ public class RunActivity extends BaseActivity implements View.OnClickListener {
         stopRunButton.setOnClickListener(this);
         finishRunButton.setOnClickListener(this);
         toMapCard.setOnClickListener(this);
+        startAnimation();
+        finishRunButton.setListener(new ProgressButton.ProgressButtonFinishCallback() {
+            @Override
+            public void onFinish() {
+            }
+
+            @Override
+            public void onCancel() {
+            }
+        });
         //ColorStateList stopColor = ContextCompat.getColorStateList(getApplicationContext(), R.color.red);
         //isRunColor = ContextCompat.getColorStateList(getApplicationContext(), R.color.green);
-//        stopRunButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                if (isRun == true) {
-//                    isRun = false;
-//                    //startRunButton.setBackgroundTintList(stopColor);
-//                    stopRunButton.hide();
-//                    startRunButton.show();
-//                    finishRunButton.show();
-//                }else if(isRun == false){
-//                    isRun = true;
-//                    stopRunButton.show();
-//                    startRunButton.hide();
-//                    finishRunButton.hide();
-//                }
-//            }
-//        });
     }
 
     @Override
@@ -59,7 +67,6 @@ public class RunActivity extends BaseActivity implements View.OnClickListener {
                 break;
             }
             case R.id.stopRunButton:
-            case R.id.finishRunButton:
             case R.id.startRunButton:{
                 Log.d("11111", "!11");
                 if (isRun == true) {
@@ -78,10 +85,70 @@ public class RunActivity extends BaseActivity implements View.OnClickListener {
                 }
                 break;
             }
+            case R.id.finishRunButton:
+                break;
             case R.id.toMapCard:
                 Intent intent = new Intent(this, RunningActivity.class);
                 startActivity(intent);
                 break;
         }
+    }
+    private void startAnimation(){
+        constraintLayout_run.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                constraintLayout_run.setVisibility(View.INVISIBLE);
+            }
+        },1000);
+        //stopRunButton.setVisibility(View.INVISIBLE);
+        ObjectAnimator back_animator0 = ObjectAnimator.ofFloat(animationBackGround,"scaleY",1,50);
+        ObjectAnimator back_animator1 = ObjectAnimator.ofFloat(animationBackGround,"scaleX",1,50);
+        ObjectAnimator animator0 = ObjectAnimator.ofFloat(animationText, "scaleY", 1f, 3f, 5f,7f,5f,3f,1f);
+        ObjectAnimator animator1 = ObjectAnimator.ofFloat(animationText, "scaleX", 1f, 3f, 5f,7f,5f,3f,1f);
+        AnimatorSet backAnimator = new AnimatorSet();
+        backAnimator.play(back_animator0).with(back_animator1);
+        backAnimator.setDuration(2500);
+        backAnimator.start();
+        AnimatorSet animatorSet0 = new AnimatorSet();
+        animatorSet0.play(animator0).with(animator1);
+        animatorSet0.setDuration(1000);
+        animatorSet0.start();
+        AnimatorSet animatorSet1 = animatorSet0.clone();
+        AnimatorSet animatorSet2 = animatorSet1.clone();
+        AnimatorSet animatorSet3 = animatorSet1.clone();
+        animatorSet0.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                animationText.setText("2");
+                animatorSet1.start();
+            }
+        });
+        animatorSet1.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                animationText.setText("1");
+                animatorSet2.start();
+            }
+        });
+        animatorSet2.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                animationText.setText("Go!");
+                animatorSet3.start();
+            }
+        });
+        animatorSet3.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                animationText.setVisibility(View.GONE);
+                animationBackGround.setVisibility(View.GONE);
+                stopRunButton.setVisibility(View.VISIBLE);
+                constraintLayout_run.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
